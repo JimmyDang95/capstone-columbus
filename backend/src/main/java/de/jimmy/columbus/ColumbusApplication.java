91745a22fcef93f837d79df08d9f15c740027395
JimmyDang95 <jimmydang12@hotmail.de>
2021-03-14 17:29:37 +0100

    Init backend

---
+++ backend/src/main/java/de/jimmy/columbus/ColumbusApplication.java
@@ -0,0 +1,13 @@
+package de.jimmy.columbus;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ColumbusApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ColumbusApplication.class, args);
+    }
+
+}
---
+++ backend/src/test/java/de/jimmy/columbus/ColumbusApplicationTests.java
@@ -0,0 +1,13 @@
+package de.jimmy.columbus;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ColumbusApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
