1e589d4e42d9a265b083225fd0b4f6a72733a738
JimmyDang95 <jimmydang12@hotmail.de>
2021-04-14 17:14:51 +0200

    Added brackets

--- backend/src/main/java/de/jimmy/columbus/config/ReactRoutingForwarding.java
+++ backend/src/main/java/de/jimmy/columbus/config/ReactRoutingForwarding.java
@@ -4,7 +4,7 @@ import org.springframework.web.bind.annotation.RequestMapping;
 
 public class ReactRoutingForwarding {
 
-    @RequestMapping(value = "/**/{path:[^\\.]*}")
+    @RequestMapping(value = {"/**/{path:[^\\.]*}"})
     public String forwardToRoutUrl() {
         return "forward:/";
     }
